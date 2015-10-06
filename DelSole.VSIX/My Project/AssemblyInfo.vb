b1c81560249f251250eb07aa930b14e552fdd899
Alessandro Del Sole <alessandro.delsole@visual-basic.it>
2015-10-06 23:06:20 +0200

    Minor fixes

--- DelSole.VSIX/My Project/AssemblyInfo.vb
+++ DelSole.VSIX/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("85dd2f67-9660-47fc-8323-501a2ea99fab")> 
+<Assembly: Guid("85dd2f67-9660-47fc-8323-501a2ea99fab")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.0.0.1")>
+<Assembly: AssemblyFileVersion("1.0.0.1")>
