b1e949767591f93fad0c98e13d9c8b6aa8212940
Alessandro Del Sole <alessandro.delsole@visual-basic.it>
2015-10-08 19:14:19 +0200

    Added demo apps for VB and C#

---
+++ SampleApp/Module1.vb
@@ -0,0 +1,30 @@
+﻿Imports DelSole.VSIX
+Module Module1
+
+    Sub Main()
+        'Create a SnippetInfo for each code snippet you want to package
+        Dim Snippet1 As New SnippetInfo
+        Snippet1.SnippetFileName = "UnprotectDocument.snippet"
+        Snippet1.SnippetPath = "C:\MySnippets"
+        Snippet1.SnippetLanguage = SnippetInfo.GetSnippetLanguage(Snippet1.SnippetPathName)
+        Snippet1.SnippetDescription = SnippetInfo.GetSnippetDescription(Snippet1.SnippetFileName)
+
+        'Create a new package
+        Dim Vsix As New VSIXPackage
+
+        'Populate the collection of snippets
+        Vsix.CodeSnippets.Add(Snippet1)
+
+        'Set package metadata information
+        Vsix.Tags = "Word"
+        Vsix.PackageAuthor = "Alessandro Del Sole"
+        Vsix.PackageDescription = "A test VSIX package with snippets"
+        Vsix.License = "C:\temp\MIT_License.txt"
+        Vsix.MoreInfoURL = "https://github.com/alessandrodelsole/delsolevsix"
+        'Assign other properties here...
+
+        'Go build it!
+        Vsix.Build("C:\temp\Sample.vsix")
+    End Sub
+
+End Module
---
+++ SampleApp_CS/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DelSole.VSIX;
+
+namespace SampleApp_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a SnippetInfo for each code snippet you want to package
+            var Snippet1 = new SnippetInfo();
+            Snippet1.SnippetFileName = "UnprotectDocument.snippet";
+            Snippet1.SnippetPath = "C:\\MySnippets";
+            Snippet1.SnippetLanguage = SnippetInfo.GetSnippetLanguage(Snippet1.SnippetPathName);
+            Snippet1.SnippetDescription = SnippetInfo.GetSnippetDescription(Snippet1.SnippetFileName);
+
+            //Create a new package
+            var Vsix = new VSIXPackage();
+
+            //Populate the collection of snippets
+            Vsix.CodeSnippets.Add(Snippet1);
+
+            //Set package metadata information
+            Vsix.Tags = "Word";
+            Vsix.PackageAuthor = "Alessandro Del Sole";
+            Vsix.PackageDescription = "A test VSIX package with snippets";
+            Vsix.License = "C:\\temp\\MIT_License.txt";
+            Vsix.MoreInfoURL = "https://github.com/alessandrodelsole/delsolevsix";
+            //Assign other properties here...
+
+            //Go build it!
+            Vsix.Build("C:\temp\Sample.vsix");
+        }
+    }
+}
