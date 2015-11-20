7c3fbd2c8b5245c26d5f94a843cceaf6dbc5b96d
Alessandro Del Sole <alessandro.delsole@visual-basic.it>
2015-11-21 00:44:58 +0100

    Added a CodeSnippet class to help generating .snippet files (differs from SnippetInfo which serves for .Vsix generation).

---
+++ DelSole.VSIX/Snippet_ObjectModel/CodeSnippet.vb
@@ -0,0 +1,45 @@
+﻿Imports System.ComponentModel
+
+Namespace SnippetTools
+    ''' <summary>
+    ''' Represent the properties of a code snippet and is used for code snippet files generation. 
+    ''' It differs from <seealso cref="SnippetInfo"/> which instead serves as a building block for generating .Vsix packages
+    ''' </summary>
+    Public Class CodeSnippet
+
+        <Category("Properties")>
+        <DisplayName("Author")>
+        <Description("Author of code snippet. This is a required value.")>
+        Public Property Author As String
+
+        <Category("Properties")>
+        <DisplayName("Title")>
+        <Description("Title of code snippet. This is a required value.")>
+        Public Property Title As String
+
+        <Category("Properties")>
+        <DisplayName("Description")>
+        <Description("Description of code snippet. This is a required value.")>
+        Public Property Description As String
+
+        <Category("Properties")>
+        <DisplayName("HelpUrl")>
+        <Description("URL where users can find help. This is an optional value.")>
+        Public Property HelpUrl As String
+
+        <Category("Properties")>
+        <DisplayName("Shortcut")>
+        <Description("Keyboard shortcut for IntelliSense. This is an optional value.")>
+        Public Property Shortcut As String
+
+        <Category("Properties")>
+        <DisplayName("Keywords")>
+        <Description("Comma separated list of keywords that help identifying a code snippet.")>
+        Public Property Keywords As String
+
+        <Category("Properties")>
+        <DisplayName("Kind")>
+        <Description("Determine the kind of code snippet.")>
+        Public Property Kind As CodeSnippetKinds
+    End Class
+End Namespace
