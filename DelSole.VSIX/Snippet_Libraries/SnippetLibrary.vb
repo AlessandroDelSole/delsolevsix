2308c21c407a8c29d08a817a50a71dde30e22623
Alessandro Del Sole <alessandro.delsole@visual-basic.it>
2016-01-02 13:25:44 +0100

    Implemented BackupLibraryToZip method. Improved LoadLibrary

--- DelSole.VSIX/Snippet_Libraries/SnippetLibrary.vb
+++ DelSole.VSIX/Snippet_Libraries/SnippetLibrary.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Collections.ObjectModel
 Imports System.ComponentModel
+Imports Ionic.Zip
 
 Namespace SnippetTools
     Public Class SnippetLibrary
@@ -40,10 +41,12 @@ Namespace SnippetTools
 
                 Dim fileNames As New List(Of String)
                 For Each folder In Me.Folders
-                    Dim files = IO.Directory.EnumerateFiles(folder.FolderName)
-                    For Each fileName In files
-                        fileNames.Add(fileName)
-                    Next
+                    Dim files = IO.Directory.EnumerateFiles(folder.FolderName).Where(Function(f) IO.Path.GetExtension(f).ToLowerInvariant.Contains("snippet") Or IO.Path.GetExtension(f).ToLowerInvariant = ".json")
+                    If files.Any Then
+                        For Each fileName In files
+                            fileNames.Add(fileName)
+                        Next
+                    End If
                 Next
                 Return fileNames.AsEnumerable
             End Get
@@ -67,16 +70,43 @@ Namespace SnippetTools
         ''' Load a snippet library list from disk. Make sure you load a list that was previously saved with the <seealso cref="SaveLibrary(String)"/> method.
         ''' </summary>
         ''' <param name="pathName"></param>
+        ''' <remarks>If a folder in the library is not found on disk, then it's not added to the snippet library</remarks>
         Public Sub LoadLibrary(pathName As String)
             Dim doc = XDocument.Load(pathName)
             Dim query = From fold In doc...<Folder>
                         Select fold.@FolderName
 
             For Each element In query
-                Dim folder As New SnippetFolder
-                folder.FolderName = element
-                Me.Folders.Add(folder)
+                If IO.Directory.Exists(element) Then
+                    Dim folder As New SnippetFolder
+                    folder.FolderName = element
+                    Me.Folders.Add(folder)
+                End If
             Next
+        End Sub
+
+        ''' <summary>
+        ''' Create a zip backup archive containing all the code snippet files in the library, maintaining the directory tree
+        ''' </summary>
+        ''' <param name="zipName"></param>
+        Public Sub BackupLibraryToZip(zipName As String)
+            Dim zip As New ZipFile(zipName)
+            Try
+                For Each fold In Folders
+                    Dim files = IO.Directory.EnumerateFiles(fold.FolderName).
+                                Where(Function(f) IO.Path.GetExtension(f).ToLower = ".snippet" _
+                                Or IO.Path.GetExtension(f).ToLower = ".json")
+
+                    zip.AddFiles(files)
+                Next
+
+                zip.Save()
+            Catch ex As ArgumentException
+                If IO.File.Exists(zipName) Then IO.File.Delete(zipName)
+                BackupLibraryToZip(zipName)
+            Catch ex As Exception
+                Throw
+            End Try
         End Sub
     End Class
 End Namespace
